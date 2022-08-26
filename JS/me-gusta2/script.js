b75f8a3fef8d667e245a7a7091f56678a500b089
DeizyRozas <dzrozas@gmail.com>
2022-08-26 18:37:05 -0400

    tarea validada y terminada

---
+++ JS/me-gusta2/script.js
@@ -0,0 +1,11 @@
+function like (){
+    document.querySelector(".count").innerText++;
+}
+
+function like1 (){
+    document.querySelector(".count1").innerText++;
+}
+
+function like2 (){
+    document.querySelector(".count2").innerText++;
+}
