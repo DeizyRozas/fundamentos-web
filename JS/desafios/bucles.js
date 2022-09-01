b79e5e1b148b068dea68b0e8e9519bf651ad3afc
DeizyRozas <dzrozas@gmail.com>
2022-09-01 12:22:35 -0400

    bucles

---
+++ JS/desafios/bucles.js
@@ -0,0 +1,40 @@
+for(var i=1; i<=20; i++){
+    console.log(i);
+}
+
+
+
+for(var i=1; i<=100; i++){
+    if(i%3==0){
+        console.log(i);
+    }
+}
+
+
+
+for (var i=4; i>-4; i-=1.5){
+    console.log(i);
+}
+
+
+
+
+
+var sum=0;
+
+for (var i=1; i<=100; i++){
+    sum = sum+i;
+}
+
+console.log(sum);
+
+
+
+
+var factorial=1;
+
+for (var i=1; i<=12; i++){
+    factorial = factorial * i;
+}
+
+console.log(factorial);
