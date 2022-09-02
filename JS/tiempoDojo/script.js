dc7b687cee6e2a16acdc70a6567e89ecade786aa
DeizyRozas <dzrozas@gmail.com>
2022-09-01 20:11:30 -0400

    validado y terminado

--- JS/tiempoDojo/script.js
+++ JS/tiempoDojo/script.js
@@ -11,72 +11,29 @@ function dismiss(){
 
 
 
-// TEMPERATURAS POR DIA
-// var hoy = [24, 18];
-// var mañana= [27, 19];
-// var viernes=[21, 16];
-// var sabado= [26, 21];
-
-// var 4dias = [{24, 18}, {}, {},{}]
-
-
-// var arrTemp= document.querySelectorAll(".celsius");
 
 function selectTemp(){
     var arrTemp = document.querySelectorAll(".celsius");
     var unidadDeMedida = document.getElementById("temperature");
     unidadDeMedida= unidadDeMedida.value;
 
-    if(unidadDeMedida==kelvin){
-        console.log(unidadDeMedida);
-    }
-
-    // for (var i = 0; i < arrTemp.length; i++) {
-    //     var  temp= arrTemp[i].innerText;
-    //     temp=Number(temp);
-        
-    // }
-
-
-    // hoy[0] = hoy[0] * 9/5 + 32;
-    // var gradosfarenheit = hoy[0];
-    // console.log(gradosfarenheit);
     
+
+    for (var i = 0; i < arrTemp.length; i++) {
+        var  temp= arrTemp[i].innerText;
+        temp=Number(temp);
+
+        if(unidadDeMedida=="farenheit"){
+        
+            var conversion = temp * 9/5 + 32;
+        }
+
+        else{
+            temp = (temp - 32) * 5/9;
+            var conversion = Math.round(temp);
+        }
+
+        arrTemp[i].innerText= conversion;
+    }
 }
 
-
-
-
-
-
-
-
-// function selectTemp(){
-//     celsiusafarenheit();
-// }
-
-//     
-// }
-
-
-
-    
-
-
-
-    // console.log(document.getElementById("temperature").value);
-    // 
-    // var conversionFarenheit = celsius * 9/5 + 32;
-
-    
-
-    // var unidadDeMedida = document.querySelector("#temperature").value;
-    // if (unidadDeMedida == kelvin){
-        
-    //     hoy[0] = conversionKelvin;
-        
-
-
-
-
-// celsiusakelvin();
